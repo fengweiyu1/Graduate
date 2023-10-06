282306c85a9149f923ffdb8a0a56b7f830ffbe28
fengweiyu1 <45956429+fengweiyu1@users.noreply.github.com>
2023-10-06 02:13:20 +0100

    new

---
+++ leetcode/add_two_numbers.py
@@ -0,0 +1,67 @@
+"""
+add_two_numbers.py
+
+This is a solution to the add two numbers problem on leetcode.com
+
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+"""
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # create a new linked list to store the sum
+        l3 = ListNode()
+        # create a pointer to the head of the new linked list
+        l3_head = l3
+        # create a variable to store the carry
+        carry = 0
+        # loop through the two linked lists
+        while l1 or l2:
+            # get the values of the two nodes
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            # calculate the sum of the two values
+            sum = l1_val + l2_val + carry
+            # calculate the carry
+            carry = sum // 10
+            # calculate the value of the current node
+            l3.val = sum % 10
+            # move to the next node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            # check if there is a next node
+            if l1 or l2:
+                # create a new node
+                l3.next = ListNode()
+                # move to the next node
+                l3 = l3.next
+
+        # check if there is a carry
+        if carry:
+            # create a new node
+            l3.next = ListNode()
+            # move to the next node
+            l3 = l3.next
+            # set the value of the node to the carry
+            l3.val = carry
+
+        # return the head of the new linked list
+
+        return l3_head
+    
+if __name__ == "__main__":
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
+    l2 = ListNode(5, ListNode(6, ListNode(4)))
+    sol = Solution()
+    result = sol.addTwoNumbers(l1, l2)
+    print(result.val)
--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,51 +1,68 @@
 # this is a solution to the two sum problem on leetcode.com
+"""
+author: Levi
 
+title: Two Sum
+
+problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+:Easy
+"""
+
+
+
+from typing import List
 
 class Solution:
-    def twoSum(self, nums, target):
-        # create a dictionary to store the values of the array
-        # as keys and the indices as values
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            # create a dictionary to store the values of the array
+            # as keys and the indices as values
         num_dict = {}
         for i in range(len(nums)):
             num_dict[nums[i]] = i
 
-        # loop through the array
+            # loop through the array
         for i in range(len(nums)):
-            # check if the complement of the current number is in the dictionary
+                # check if the complement of the current number is in the dictionary
             complement = target - nums[i]
             if complement in num_dict:
-                # check if the complement is not the current number
-                if num_dict[complement] != i: # this is to prevent using the same number twice
-                    # return the indices of the two numbers
+                    # check if the complement is not the current number
+                if (
+                    num_dict[complement] != i
+                    ):  # this is to prevent using the same number twice
+                        # return the indices of the two numbers
                     return [i, num_dict[complement]]
-                
-        # if no solution is found, return an empty array
-        return []
-    
-    def twoSum2(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                # check if the sum of the two numbers is equal to the target
-                if nums[i] + nums[j] == target:
-                    # return the indices of the two numbers
-                    return [i, j]
-                 # if all number combinations are checked and no solution is found, return an empty array
-        return []
-    
 
-if __name__ == '__main__':
-    # create an instance of the Solution class
+            # if no solution is found, return an empty array
+        return []
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                    # check if the sum of the two numbers is equal to the target
+                if nums[i] + nums[j] == target:
+                        # return the indices of the two numbers
+                    return [i, j]
+                    # if all number combinations are checked and no solution is found, return an empty array
+        return []
+
+if __name__ == "__main__":
+        # create an instance of the Solution class
     sol = Solution()
 
-    # create a list of numbers
+        # create a list of numbers
     nums = [2, 7, 11, 15]
-    # create a target number
+        # create a target number
     target = 9
 
-    # call the twoSum function
+        # call the twoSum function
     result = sol.twoSum(nums, target)
     print(result)
 
-    # call the twoSum2 function
+        # call the twoSum2 function
     result = sol.twoSum2(nums, target)
     print(result)
